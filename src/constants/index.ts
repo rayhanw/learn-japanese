9e86aa0a7441e7e8cf4f13af2874734c61f9aeb5
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-09-23 21:01:44 +0800

    add unfinished Hiragana mapping

---
+++ src/constants/index.ts
@@ -0,0 +1,125 @@
+export const HIRAGANA_MAPPING = [
+	{
+		main: {
+			vowels: {
+				a: "あ",
+				i: "い",
+				u: "う",
+				e: "え",
+				o: "お"
+			},
+			kGroup: {
+				ka: "か",
+				ki: "き",
+				ku: "く",
+				ke: "け",
+				ko: "こ"
+			},
+			sGroup: {
+				sa: "さ",
+				shi: "し",
+				su: "す",
+				se: "せ",
+				so: "そ"
+			},
+			tGroup: {
+				ta: "た",
+				chi: "ち",
+				tsu: "つ",
+				te: "て",
+				to: "と"
+			},
+			nGroup: {
+				na: "な",
+				ni: "に",
+				nu: "ぬ",
+				ne: "ね",
+				no: "の"
+			},
+			hGroup: {
+				ha: "は",
+				hi: "ひ",
+				fu: "ふ",
+				he: "へ",
+				ho: "ほ"
+			},
+			mGroup: {
+				ma: "ま",
+				mi: "み",
+				mu: "む",
+				me: "め",
+				mo: "も"
+			},
+			yGroup: {
+				ya: "や",
+				yu: "ゆ",
+				yo: "よ"
+			},
+			rGroup: {
+				ra: "ら",
+				ri: "り",
+				ru: "る",
+				re: "れ",
+				ro: "ろ"
+			},
+			wGroup: {
+				wa: "わ",
+				wo: "を"
+			},
+			others: {
+				n: "ん"
+			}
+		},
+		dakuten: {
+			gGroup: {
+				ga: "が",
+				gi: "ぎ",
+				gu: "ぐ",
+				ge: "げ",
+				go: "ご"
+			},
+			zGroup: {
+				za: "ざ",
+				zi: "じ",
+				zu: "ず",
+				ze: "ぜ",
+				zo: "ぞ"
+			},
+			dGroup: {
+				da: "だ",
+				di: "ぢ",
+				du: "づ",
+				de: "で",
+				do: "ど"
+			},
+			bGroup: {
+				ba: "ば",
+				bi: "び",
+				bu: "ぶ",
+				be: "べ",
+				bo: "ぼ"
+			},
+			pGroup: {
+				pa: "ぱ",
+				pi: "ぴ",
+				pu: "ぷ",
+				pe: "ぺ",
+				po: "ぽ"
+			}
+		},
+		dakutenCombination: {
+			kyGroup: {},
+			shGroup: {},
+			chGroup: {},
+			nyGroup: {},
+			hyGroup: {},
+			myGroup: {},
+			ryGroup: {},
+			gyGroup: {},
+			jGroup: {},
+			dyGroup: {},
+			byGroup: {},
+			pyGroup: {}
+		}
+	}
+];
