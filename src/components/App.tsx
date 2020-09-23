360ed0acfc5973f53a04af91959b9bb15396ec61
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-09-23 21:01:36 +0800

    folder structure setup

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Learn Japanese!</h1>
+			</header>
+		</div>
+	);
+};
+
+export default App;
