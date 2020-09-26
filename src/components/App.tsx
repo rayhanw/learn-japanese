e8ca115325556227ccf9aab99c950b1a1f3f719c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-09-26 14:18:43 +0800

    wrap app in provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,34 +5,37 @@ import { Navbar } from "./Navbar";
 import { Hiragana } from "../pages/Hiragana";
 import { Katakana } from "../pages/Katakana";
 import { Layout } from "./Layout";
+import { GlobalStateProvider } from "../contexts/globalState";
 
 export const App: React.FC = () => {
 	return (
 		<Router>
-			<Layout>
-				<header>
-					<Navbar />
-					<h1>Type Romaji for the Hiragana That You Know!</h1>
-					<ul>
-						{INSTRUCTIONS.map((text, i) => (
-							<li key={i}>{text}</li>
-						))}
-					</ul>
-				</header>
-				<main id="content">
-					<Switch>
-						<Route path="/hiragana">
-							<Hiragana />
-						</Route>
-						<Route path="/katakana">
-							<Katakana />
-						</Route>
-						<Route path="/">
-							<Hiragana />
-						</Route>
-					</Switch>
-				</main>
-			</Layout>
+			<GlobalStateProvider>
+				<Layout>
+					<header>
+						<Navbar />
+						<h1>Type Romaji for the Hiragana That You Know!</h1>
+						<ul>
+							{INSTRUCTIONS.map((text, i) => (
+								<li key={i}>{text}</li>
+							))}
+						</ul>
+					</header>
+					<main id="content">
+						<Switch>
+							<Route path="/hiragana">
+								<Hiragana />
+							</Route>
+							<Route path="/katakana">
+								<Katakana />
+							</Route>
+							<Route path="/">
+								<Hiragana />
+							</Route>
+						</Switch>
+					</main>
+				</Layout>
+			</GlobalStateProvider>
 		</Router>
 	);
 };
