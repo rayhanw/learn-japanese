42ac62a5d38752d574f40888dd2ec61e707e762d
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-10-03 15:18:10 +0800

    add functionality to Quiz page

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,8 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useGlobalStateContext } from "../contexts/globalState";
+import { HIRAGANA_MAPPING } from "../constants";
+
+const mapActiveToValues = (
+	activeKata: string[],
+	hiraganaKey: "main" | "dakuten" | "dakutenCombination"
+) => {
+	const kataToBeInputted: Record<string, string>[] = [];
+	const hiraganaKeys = Object.keys(HIRAGANA_MAPPING[hiraganaKey]);
+
+	activeKata.forEach(kata => {
+		if (hiraganaKeys.includes(kata)) {
+			const words = (HIRAGANA_MAPPING[hiraganaKey] as any)[kata];
+			Object.keys(words).forEach(word => {
+				kataToBeInputted.push({ [word]: words[word] });
+			});
+		}
+	});
+
+	return kataToBeInputted;
+};
 
 export const Quiz: React.FC = () => {
-	const { dispatch } = useGlobalStateContext();
+	const {
+		dispatch,
+		state: { kata }
+	} = useGlobalStateContext();
+	const [activeKata, setActiveKata] = useState<Record<string, string>[]>([]);
 
 	useEffect(() => {
 		return () => {
@@ -10,9 +34,38 @@ export const Quiz: React.FC = () => {
 		};
 	}, [dispatch]);
 
+	useEffect(() => {
+		setActiveKata(prevState => {
+			const mainHiragana = mapActiveToValues(kata, "main");
+			const dakutenHiragana = mapActiveToValues(kata, "dakuten");
+			const combinationHiragana = mapActiveToValues(kata, "dakutenCombination");
+
+			return [
+				...mainHiragana,
+				...dakutenHiragana,
+				...combinationHiragana,
+				...prevState
+			];
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<div id="quizPage">
 			<h1>Quiz</h1>
+			<section>
+				{activeKata.map((kata, i) => {
+					const key = Object.keys(kata)[0];
+
+					return (
+						<div className="flex" key={i}>
+							<p>{key}</p>
+							<p>:</p>
+							<p>{kata[key]}</p>
+						</div>
+					);
+				})}
+			</section>
 		</div>
 	);
 };
