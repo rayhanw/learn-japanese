c69d71da27bb591ec0e75d85b026afc539438be8
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-10-05 14:43:07 +0800

    built result page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,9 +1,73 @@
 import React from "react";
+import { useGlobalStateContext } from "../contexts/globalState";
 
 export const Result: React.FC = () => {
+	const {
+		state: { result }
+	} = useGlobalStateContext();
+	const { main, dakuten, dakutenCombination } = result;
+
 	return (
 		<div id="resultPage">
-			<h1>Your result</h1>
+			<div className="pageGrid">
+				<div>
+					<h2 className="title">Main Hiragana</h2>
+					<div className="flex flexWrap">
+						{Object.keys(main).map((key, i) => (
+							<div className="resultCard" key={i}>
+								<div className="whiteText">
+									<p className="noMargin">{key}</p>
+								</div>
+								<div className="flex">
+									{main[key] ? (
+										<input type="radio" />
+									) : (
+										<div className="whiteText">x: 1</div>
+									)}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div>
+					<h2 className="title">Dakuten Hiragana</h2>
+					<div className="flex flexWrap">
+						{Object.keys(dakuten).map((key, i) => (
+							<div className="resultCard" key={i}>
+								<div className="whiteText">
+									<p className="noMargin">{key}</p>
+								</div>
+								<div className="flex">
+									{dakuten[key] ? (
+										<input type="radio" />
+									) : (
+										<div className="whiteText">x: 1</div>
+									)}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div>
+					<h2 className="title">Combination Hiragana</h2>
+					<div className="flex flexWrap">
+						{Object.keys(dakutenCombination).map((key, i) => (
+							<div className="resultCard" key={i}>
+								<div className="whiteText">
+									<p className="noMargin">{key}</p>
+								</div>
+								<div className="flex">
+									{dakutenCombination[key] ? (
+										<input type="radio" />
+									) : (
+										<div className="whiteText">x: 1</div>
+									)}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
