899cf96764414e66a213a8d5112add0ade34052a
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-10-09 13:01:26 +0800

    split code /constants

---
+++ src/constants/mappings.ts
@@ -0,0 +1,339 @@
+export const HIRAGANA_MAPPING = {
+	main: {
+		a: {
+			a: "あ",
+			i: "い",
+			u: "う",
+			e: "え",
+			o: "お"
+		},
+		ka: {
+			ka: "か",
+			ki: "き",
+			ku: "く",
+			ke: "け",
+			ko: "こ"
+		},
+		sa: {
+			sa: "さ",
+			shi: "し",
+			su: "す",
+			se: "せ",
+			so: "そ"
+		},
+		ta: {
+			ta: "た",
+			chi: "ち",
+			tsu: "つ",
+			te: "て",
+			to: "と"
+		},
+		na: {
+			na: "な",
+			ni: "に",
+			nu: "ぬ",
+			ne: "ね",
+			no: "の"
+		},
+		ha: {
+			ha: "は",
+			hi: "ひ",
+			fu: "ふ",
+			he: "へ",
+			ho: "ほ"
+		},
+		ma: {
+			ma: "ま",
+			mi: "み",
+			mu: "む",
+			me: "め",
+			mo: "も"
+		},
+		ya: {
+			ya: "や",
+			yu: "ゆ",
+			yo: "よ"
+		},
+		ra: {
+			ra: "ら",
+			ri: "り",
+			ru: "る",
+			re: "れ",
+			ro: "ろ"
+		},
+		wa: {
+			wa: "わ",
+			wo: "を",
+			n: "ん"
+		}
+	},
+	dakuten: {
+		ga: {
+			ga: "が",
+			gi: "ぎ",
+			gu: "ぐ",
+			ge: "げ",
+			go: "ご"
+		},
+		za: {
+			za: "ざ",
+			zi: "じ",
+			zu: "ず",
+			ze: "ぜ",
+			zo: "ぞ"
+		},
+		da: {
+			da: "だ",
+			di: "ぢ",
+			du: "づ",
+			de: "で",
+			do: "ど"
+		},
+		ba: {
+			ba: "ば",
+			bi: "び",
+			bu: "ぶ",
+			be: "べ",
+			bo: "ぼ"
+		},
+		pa: {
+			pa: "ぱ",
+			pi: "ぴ",
+			pu: "ぷ",
+			pe: "ぺ",
+			po: "ぽ"
+		}
+	},
+	dakutenCombination: {
+		kya: {
+			kya: "きゃ",
+			kyu: "きゅ",
+			kyo: "きょ"
+		},
+		sha: {
+			sha: "しゃ",
+			shu: "しゅ",
+			sho: "しょ"
+		},
+		cha: {
+			cha: "ちゃ",
+			chu: "ちゅ",
+			cho: "ちょ"
+		},
+		nya: {
+			nya: "にゃ",
+			nyu: "にゅ",
+			nyo: "にょ"
+		},
+		hya: {
+			hya: "ひゃ",
+			hyu: "ひゅ",
+			hyo: "ひょ"
+		},
+		mya: {
+			mya: "みゃ",
+			myu: "みゅ",
+			myo: "みょ"
+		},
+		rya: {
+			rya: "りゃ",
+			ryu: "りゅ",
+			ryo: "りょ"
+		},
+		gya: {
+			gya: "ぎゃ",
+			gyu: "ぎゅ",
+			gyo: "ぎょ"
+		},
+		ja: {
+			ja: "じゃ",
+			ju: "じゅ",
+			jo: "じょ"
+		},
+		dya: {
+			dya: "ぢゃ",
+			dyu: "ぢゅ",
+			dyo: "ぢょ"
+		},
+		bya: {
+			bya: "びゃ",
+			byu: "びゅ",
+			byo: "びょ"
+		},
+		pya: {
+			pya: "ぴゃ",
+			pyu: "ぴゅ",
+			pyo: "ぴょ"
+		}
+	}
+};
+
+export const KATAKANA_MAPPING = {
+	main: {
+		a: {
+			a: "ア",
+			i: "イ",
+			u: "ウ",
+			e: "エ",
+			o: "オ"
+		},
+		ka: {
+			ka: "カ",
+			ki: "キ",
+			ku: "ク",
+			ke: "ケ",
+			ko: "コ"
+		},
+		sa: {
+			sa: "サ",
+			shi: "シ",
+			su: "ス",
+			se: "セ",
+			so: "ソ"
+		},
+		ta: {
+			ta: "タ",
+			chi: "チ",
+			tsu: "ツ",
+			te: "テ",
+			to: "ト"
+		},
+		na: {
+			na: "カ",
+			ni: "キ",
+			nu: "ク",
+			ne: "ケ",
+			no: "コ"
+		},
+		ha: {
+			ha: "ハ",
+			hi: "ヒ",
+			fu: "フ",
+			he: "ヘ",
+			ho: "ホ"
+		},
+		ma: {
+			ma: "ハ",
+			mi: "ヒ",
+			mu: "フ",
+			me: "ヘ",
+			mo: "ホ"
+		},
+		ya: {
+			ya: "ヤ",
+			yu: "ユ",
+			yo: "ヨ"
+		},
+		ra: {
+			ra: "ラ",
+			ri: "リ",
+			ru: "ル",
+			re: "レ",
+			ro: "ロ"
+		},
+		wa: {
+			wa: "ワ",
+			wo: "ヲ",
+			n: "ン"
+		}
+	},
+	dakuten: {
+		ga: {
+			ga: "ガ",
+			gi: "ギ",
+			gu: "グ",
+			ge: "ゲ",
+			go: "ゴ"
+		},
+		za: {
+			za: "ザ",
+			ji: "ジ",
+			zu: "ズ",
+			ze: "ゼ",
+			zo: "ゾ"
+		},
+		da: {
+			da: "ダ",
+			di: "ジ",
+			du: "ズ",
+			de: "デ",
+			do: "ド"
+		},
+		ba: {
+			ba: "バ",
+			bi: "ビ",
+			bu: "ブ",
+			be: "ベ",
+			bo: "ボ"
+		},
+		pa: {
+			pa: "パ",
+			pi: "ピ",
+			pu: "プ",
+			pe: "ペ",
+			po: "ポ"
+		}
+	},
+	dakutenCombination: {
+		kya: {
+			kya: "キャ",
+			kyu: "キュ",
+			kyo: "キョ"
+		},
+		hya: {
+			hya: "ヒャ",
+			hyu: "ヒュ",
+			hyo: "ヒョ"
+		},
+		jya: {
+			jya: "ジャ",
+			jyu: "ジュ",
+			jyo: "ジョ"
+		},
+		sha: {
+			sha: "シャ",
+			shu: "シュ",
+			sho: "ショ"
+		},
+		mya: {
+			mya: "ミャ",
+			myu: "ミュ",
+			myo: "ミョ"
+		},
+		dzya: {
+			dzya: "ヂャ",
+			dzyu: "ヂュ",
+			dzyo: "ヂョ"
+		},
+		cha: {
+			cha: "チャ",
+			chu: "チュ",
+			cho: "チョ"
+		},
+		rya: {
+			rya: "リャ",
+			ryu: "リュ",
+			ryo: "リョ"
+		},
+		bya: {
+			bya: "ビャ",
+			byu: "ビュ",
+			byo: "ビョ"
+		},
+		pya: {
+			pya: "ピャ",
+			pyu: "ピュ",
+			pyo: "ピョ"
+		},
+		nya: {
+			nya: "ニャ",
+			nyu: "ニュ",
+			nyo: "ニョ"
+		},
+		gya: {
+			gya: "ギャ",
+			gyu: "ギュ",
+			gyo: "ギョ"
+		}
+	}
+};
---
+++ src/constants/others.ts
@@ -0,0 +1,22 @@
+export const INSTRUCTIONS = [
+	"Type your answer in romaji in the card's text field",
+	"Press ENTER to submit",
+	"Repeat for as many cards as you can",
+	"You can try as many times as you want",
+	'When you\'re done press the "Finish Quiz" button at the bottom'
+];
+
+export const LINKS = [
+	{
+		text: "Home",
+		url: "/"
+	},
+	{
+		text: "Hiragana",
+		url: "/hiragana"
+	},
+	{
+		text: "Katakana",
+		url: "/katakana"
+	}
+];
