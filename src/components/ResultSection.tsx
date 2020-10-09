26dfa103e9a916d40fa95e9a6db965010075ec1d
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-10-10 07:32:26 +0800

    show percentage in result page

--- src/components/ResultSection.tsx
+++ src/components/ResultSection.tsx
@@ -9,13 +9,20 @@ export const ResultSection: React.FC<ResultSectionProps> = ({
 	title,
 	content
 }) => {
-	const keys = Object.keys(content);
+  const keys = Object.keys(content);
+  const values = Object.values(content);
+  const valuesLength = values.length;
+  const trueLength = values.filter(ele => ele).length;
+  const truePercentage = ((trueLength * 100) / valuesLength).toFixed();
 
-	if (keys.length === 0) return null;
+  if (keys.length === 0) return null;
 
 	return (
 		<div>
-			<h2 className="title">{title}</h2>
+			<h2 className="title">
+        {title}
+        <span className="fontSm blackText normalWeight ml-1">{trueLength}/{values.length}{" "}({truePercentage}%)</span>
+      </h2>
 			<div className="flex flexWrap">
 				{keys.map((key, i) => (
 					<div className="resultCard" key={i}>
