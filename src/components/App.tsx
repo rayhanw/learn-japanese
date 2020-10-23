2864ae2be9f3d4214237ce3c35556514ce0d9efb
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-10-23 09:06:05 +0800

    create localStorageHook & ThemeContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from "./Layout";
 import { Navbar } from "./Navbar";
 import { Loading } from "./Loading";
 import { Footer } from "./Footer";
+import { ThemeContextProvider } from "../contexts/theme/theme";
 
 const Hiragana = lazy(() => import("../pages/Hiragana"));
 const Katakana = lazy(() => import("../pages/Katakana"));
@@ -15,33 +16,35 @@ const Landing = lazy(() => import("../pages/Landing"));
 export const App: React.FC = () => {
 	return (
 		<Router>
-			<GlobalStateProvider>
-				<Layout>
-					<Navbar />
-					<main id="content">
-						<Suspense fallback={<Loading />}>
-							<Switch>
-								<Route path="/quiz/:type">
-									<Quiz />
-								</Route>
-								<Route path="/katakana">
-									<Katakana />
-								</Route>
-								<Route path="/result/:type">
-									<Result />
-								</Route>
-								<Route path="/hiragana">
-									<Hiragana />
-								</Route>
-								<Route exact path="/">
-									<Landing />
-								</Route>
-							</Switch>
-						</Suspense>
-					</main>
-					<Footer />
-				</Layout>
-			</GlobalStateProvider>
+			<ThemeContextProvider>
+				<GlobalStateProvider>
+					<Layout>
+						<Navbar />
+						<main id="content">
+							<Suspense fallback={<Loading />}>
+								<Switch>
+									<Route path="/quiz/:type">
+										<Quiz />
+									</Route>
+									<Route path="/katakana">
+										<Katakana />
+									</Route>
+									<Route path="/result/:type">
+										<Result />
+									</Route>
+									<Route path="/hiragana">
+										<Hiragana />
+									</Route>
+									<Route exact path="/">
+										<Landing />
+									</Route>
+								</Switch>
+							</Suspense>
+						</main>
+						<Footer />
+					</Layout>
+				</GlobalStateProvider>
+			</ThemeContextProvider>
 		</Router>
 	);
 };
