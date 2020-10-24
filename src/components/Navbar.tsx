0d84a7e83cb9e7f897ef9d4eb3eec2c69e987432
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-10-24 12:49:46 +0800

    clear all on page change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { LINKS } from "../constants";
 import { Link } from "react-router-dom";
+import { LINKS } from "../constants";
 import { useThemeContext } from "../contexts/theme/theme";
+import { useGlobalStateContext } from "../contexts/globalState/globalState";
 
 export const Navbar: React.FC = () => {
 	const { theme, toggleTheme } = useThemeContext();
+	const { dispatch } = useGlobalStateContext();
 
 	return (
 		<nav>
 			<ul>
 				{LINKS.map(({ text, url }, i) => (
 					<li key={i}>
-						<Link to={url}>{text}</Link>
+						<Link to={url} onClick={() => dispatch({ type: "CLEAR_ALL" })}>
+							{text}
+						</Link>
 					</li>
 				))}
 			</ul>
