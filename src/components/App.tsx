ec9c6db8b082fd57c5075b01f37f04f8325bb5ea
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-23 15:33:43 +0800

    init i18n

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import english from "../translations/en.json";
+import indonesian from "../translations/id.json";
 import { GlobalStateProvider } from "../contexts/globalState/globalState";
 import { Layout } from "./Layout";
 import { Navbar } from "./Navbar";
@@ -12,6 +16,21 @@ const Katakana = lazy(() => import("../pages/Katakana"));
 const Quiz = lazy(() => import("../pages/Quiz"));
 const Result = lazy(() => import("../pages/Result"));
 const Landing = lazy(() => import("../pages/Landing"));
+
+i18n
+	.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		resources: {
+			en: english,
+			id: indonesian
+		},
+		lng: "en",
+		fallbackLng: "en",
+
+		interpolation: {
+			escapeValue: false
+		}
+	});
 
 export const App: React.FC = () => {
 	return (
