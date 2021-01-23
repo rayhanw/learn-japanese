9363c5224bec78fd7af63658b9ecf905cebd0d81
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-23 15:56:59 +0800

    appear LanguageSwitcher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,45 +3,27 @@ import { Link } from "react-router-dom";
 import { LINKS } from "../constants";
 import { useThemeContext } from "../contexts/theme/theme";
 import { useGlobalStateContext } from "../contexts/globalState/globalState";
-import { Language } from "../contexts/globalState/types";
-import { i18n } from "../utilities";
-
-const LANGUAGES: { code: Language; text: string }[] = [
-	{
-		code: "en",
-		text: "English"
-	},
-	{
-		code: "id",
-		text: "Bahasa Indonesia"
-	}
-];
+import { LanguageSwitcher } from "./LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 
 export const Navbar: React.FC = () => {
 	const { theme, toggleTheme } = useThemeContext();
 	const { dispatch } = useGlobalStateContext();
-
-	const handleLanguageChange = (code: Language): void => {
-		i18n.changeLanguage(code);
-	};
+	const { t } = useTranslation();
 
 	return (
 		<nav>
 			<ul>
-				{LINKS.map(({ text, url }, i) => (
+				{LINKS.map(({ code, url }, i) => (
 					<li key={i}>
 						<Link to={url} onClick={() => dispatch({ type: "CLEAR_ALL" })}>
-							{text}
+							{t(code)}
 						</Link>
 					</li>
 				))}
 			</ul>
-			<div>
-				{LANGUAGES.map(({ code, text }, i) => (
-					<button onClick={() => handleLanguageChange(code)} key={i}>
-						{text}
-					</button>
-				))}
+			<div className="navbar-togglers">
+				<LanguageSwitcher />
 				<label htmlFor={`${theme}-input`} className="switch">
 					<input
 						id={`${theme}-input`}
