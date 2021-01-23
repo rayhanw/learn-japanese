e654335e3821fb839c2d7f14da0295623991af47
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-23 15:33:50 +0800

    use t()

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Head } from "../components/Head";
+import { useTranslation } from "react-i18next";
 
 const Landing: React.FC = () => {
+	const { t } = useTranslation();
+
 	return (
 		<div id="landingPage">
 			<Head title="Home" />
-			<h1 className="textCenter title">
-				Clone of Tofugu's Learn Hiragana Quiz (+ Katakana)
-			</h1>
+			<h1 className="textCenter title">{t("title")}</h1>
 			<div className="squashedIn">
 				<p>
-					If you haven't heard about Tofugu and want to learn Japanese, please
-					take your time and read{" "}
+					{t("subtitle")}{" "}
 					<a
 						href="https://www.tofugu.com/learn-japanese/"
 						rel="noreferrer noopener"
 						target="_blank"
 					>
-						this article by Tofugu
+						{t("tofugu-link")}
 					</a>{" "}
 					first!
 				</p>
 			</div>
 			<section className="mt-3">
-				<p className="textCenter">Please choose one!</p>
+				<p className="textCenter">{t("choose")}</p>
 				<div className="flexAllCenter">
 					<Link
 						to="/hiragana"
